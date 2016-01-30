e7743c50445da877743eb1856bb09c53b8549a44
Astalos <tom.czura@gmail.com>
2016-01-30 11:13:25 +0100

    Initial commit

---
+++ app/src/main/java/com/czura/recipies/views/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.czura.recipies.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.czura.recipies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
